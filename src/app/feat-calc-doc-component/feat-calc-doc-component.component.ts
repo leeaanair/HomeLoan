279c24c10acda5b708cfc8bb352e574c150f11af
leeaanair <leeaanair@gmail.com>
2020-11-29 19:26:00 +0530

    added a homepage

---
+++ src/app/feat-calc-doc-component/feat-calc-doc-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feat-calc-doc-component',
+  templateUrl: './feat-calc-doc-component.component.html',
+  styleUrls: ['./feat-calc-doc-component.component.css']
+})
+export class FeatCalcDocComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
